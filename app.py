334a17850ce6b5f92e67b20c5d6515c37dfe66e7
Ubuntu <ubuntu@ip-172-31-29-47.ec2.internal>
2024-10-26 18:21:36 +0000

    Update files

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import os
 app = Flask(__name__)
 
 @app.route("/")
+def get_hello():
+    return "PONG!", 200
+
+@app.route("/large")
 def create_list():
     arr = [0] * (10**7)
     return arr, 200
